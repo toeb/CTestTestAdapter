e965e1a7ecd892cb21f831deddb8f9cb312bce96
Kai Nadler <kai.nadler@googlemail.com>
2015-09-21 13:38:45 +0200

    refactor: Remove CTestCase and convert to in-memory structure. Reorganize the CTestContainer to use a CTestTestfile.cmake as Source property. A CTestContainer therefore can contain more than one test case. Scanning and running of test cases is always restricted to the directory of the corresponding CTestTestfile.cmake.

--- ImplemenationCTestTestAdapter/CTestExecutor.cs
+++ ImplemenationCTestTestAdapter/CTestExecutor.cs
@@ -3,86 +3,100 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace ImplemenationCTestTestAdapter
 {
 
-  /// <summary>
-  /// this class executes a ctest TestCase by calling ctest -I 
-  /// </summary>
-  [ExtensionUri(CTestExecutor.ExecutorUriString)]
-  public class CTestExecutor : ITestExecutor
-  {
-
     /// <summary>
-    /// this identifies the testexecuter
+    /// This class executes a ctest TestCase by calling ctest -R for
+    /// each test case in the directory of the belonging CTestTestfile.cmake
+    /// source file.
     /// </summary>
-    public const string ExecutorUriString = "executor://CTestExecutor/v1";
-
-    private bool cancelled;
-
-    public void Cancel()
+    [ExtensionUri(CTestExecutor.ExecutorUriString)]
+    public class CTestExecutor : ITestExecutor
     {
-      cancelled = true;
-    }
 
-    /// <summary>
-    /// delegates to other RunTests signature
-    /// </summary>
-    /// <param name="sources"></param>
-    /// <param name="runContext"></param>
-    /// <param name="frameworkHandle"></param>
-    public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
-    {
-      var tcs = sources.Select(src => (TestCase)CTestCase.Parse(src))
-          .Where(it => it != null)
-          .ToList();
-      RunTests(tcs, runContext, frameworkHandle);
-    }
+        /// <summary>
+        /// this identifies the testexecuter
+        /// </summary>
+        public const string ExecutorUriString = "executor://CTestExecutor/v1";
 
-    /// <summary>
-    /// runs a separate ctest call for every testcase
-    /// 
-    /// @maybe use -I to run all test cases
-    /// @todo add more metadata to tests!
-    /// </summary>
-    /// <param name="tests"></param>
-    /// <param name="runContext"></param>
-    /// <param name="frameworkHandle"></param>
-    public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
-    {
-      foreach (var test in tests)
-      {
-        if (cancelled) break;
+        private bool cancelled;
 
-        CTestCase ctest = test;
-        var process = new Process();
-        process.StartInfo = new ProcessStartInfo()
+        public void Cancel()
         {
-          Arguments = "-I \"" + ctest.Number + "," + ctest.Number + ",," + ctest.Number + "\"",
-          FileName = "ctest",
-          WorkingDirectory = ctest.CMakeBinaryDir,
-          CreateNoWindow = true,
-          RedirectStandardOutput = true,
-          RedirectStandardError = true,
-          UseShellExecute = false,
-          WindowStyle = ProcessWindowStyle.Hidden
-        };
+            cancelled = true;
+        }
 
-        process.Start();
-        var output = process.StandardOutput.ReadToEnd();
+        /// <summary>
+        /// delegates to other RunTests signature
+        /// </summary>
+        /// <param name="sources"></param>
+        /// <param name="runContext"></param>
+        /// <param name="frameworkHandle"></param>
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            foreach (var source in sources)
+            {
+                var testcases = CTestHelpers.GetTestCases(source);
+                RunTests(testcases, runContext, frameworkHandle);
+            }
+        }
 
-        var exitCode = process.ExitCode;
+        /// <summary>
+        /// Runs each test using the FullyQualifiedName name using
+        /// -R option of ctest.
+        /// </summary>
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            foreach (var test in tests)
+            {
+                if (cancelled)
+                    break;
 
-        var testResult = new TestResult(test);
-        testResult.Outcome = exitCode == 0 ? TestOutcome.Passed : TestOutcome.Failed;
-        frameworkHandle.RecordResult(testResult);
+                // TODO: Need to figure out the currently selected configuration
+                var configuration = "Debug";
+                
+                var testName = test.FullyQualifiedName;
+                var process = new Process();
 
+                var fileInfo = new FileInfo(test.Source);
+                process.StartInfo = new ProcessStartInfo()
+                {
+                    Arguments = "-C " + configuration + " -R " + testName,
+                    FileName = "ctest",
+                    WorkingDirectory = fileInfo.DirectoryName,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
 
-      }
+                process.Start();
+                var output = process.StandardOutput.ReadToEnd();
+
+                var exitCode = process.ExitCode;
+
+                // TUDO: Regex to get Passed time
+                //Test project C:/data/58481/build/working
+                //Start 4: dynamic_libraries_dynamicLibrary_getFunction_not_working
+                //1/1 Test #4: dynamic_libraries_dynamicLibrary_getFunction_not_working ...   Passed    0.07 sec
+
+                //100% tests passed, 0 tests failed out of 1
+
+                //Total Test time (real) =   1.13 sec
+
+                var testResult = new TestResult(test);
+                testResult.ComputerName = Environment.MachineName;
+                
+                testResult.Outcome = exitCode == 0 ? TestOutcome.Passed : TestOutcome.Failed;
+                frameworkHandle.RecordResult(testResult);
+            }
+        }
+
+        public static Uri ExecutorUri = new Uri(ExecutorUriString);
     }
-
-    public static Uri ExecutorUri = new Uri(ExecutorUriString);
-  }
 }
--- ImplemenationCTestTestAdapter/CTestHelpers.cs
+++ ImplemenationCTestTestAdapter/CTestHelpers.cs
@@ -1,48 +1,50 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace ImplemenationCTestTestAdapter
 {
-
-  
-  public static class CTestHelpers
-  {
-    /// <summary>
-    /// returns a enumerable of CTestCases which are queried by ctest -N
-    /// </summary>
-    /// <param name="solution_dir"></param>
-    /// <returns></returns>
-    public static IEnumerable<CTestCase> GetTestCases(string solution_dir)
+    public static class CTestHelpers
     {
-      var process = new Process();
-      process.StartInfo = new ProcessStartInfo()
-      {
-        FileName = "ctest",
-        Arguments = "-N",
-        CreateNoWindow = true,
-        RedirectStandardOutput = true,
-        RedirectStandardError = true,
-        WorkingDirectory = solution_dir,
-        WindowStyle = ProcessWindowStyle.Hidden,
-        UseShellExecute = false,
-  
-      };
-  
-      process.Start();
-      
-  
-      var output = process.StandardOutput.ReadToEnd();
-  
-      var matches = Regex.Matches(output, @".*#(?<number>[1-9][0-9]*): *(?<testname>.*)");
-      foreach (var match in matches)
-      {
-        var m = match as Match;
-        var name = m.Groups["testname"].Value;
-        var number = m.Groups["number"].Value;
-        yield return new CTestCase() { CMakeBinaryDir = Path.GetFullPath(solution_dir + "\\\\"), Name = name, Number = number };
-      }
+        /// <summary>
+        /// Returns an enumerable of CTestCases which are queried by ctest -N
+        /// for a given source file which is a CTestTestfile.cmake.
+        /// </summary>
+        public static IEnumerable<TestCase> GetTestCases(string source)
+        {
+            var file = new FileInfo(source);
+            
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "ctest",
+                Arguments = "-N",
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WorkingDirectory = file.DirectoryName,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+            };
+
+            process.Start();
+
+            var output = process.StandardOutput.ReadToEnd();
+
+            var matches = Regex.Matches(output, @".*#[1-9][0-9]*: *(?<testname>\w*)");
+            foreach (var match in matches)
+            {
+                var m = match as Match;
+                var name = m.Groups["testname"].Value;
+                
+                var t = new TestCase(name, CTestExecutor.ExecutorUri, source);
+                // TODO: Add more meta information e.g. Line number, source file and
+                // some traits for grouping.
+                yield return t;
+            }
+        }
     }
-  }
 }
