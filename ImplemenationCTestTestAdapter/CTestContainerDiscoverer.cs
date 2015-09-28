89b3bdcc14b7fd003737c8faaa11b6e3d4dcb968
Kai Nadler <kai.nadler@googlemail.com>
2015-09-28 09:56:13 +0200

    fix: Versuche CTestTestfile.cmake nur zu öffnen wenn die Datei auch existiert.

--- ImplemenationCTestTestAdapter/CTestContainerDiscoverer.cs
+++ ImplemenationCTestTestAdapter/CTestContainerDiscoverer.cs
@@ -23,11 +23,13 @@ namespace ImplemenationCTestTestAdapter
 
         [Import]
         public IUnitTestStorage Storage { get; set; }
+        
         [ImportingConstructor]
         public CTestContainerDiscoverer()
         {
 
         }
+
         public Uri ExecutorUri
         {
             get { return CTestExecutor.ExecutorUri; }
@@ -50,7 +52,7 @@ namespace ImplemenationCTestTestAdapter
                 object solutionpath_o = "";
                 solution.GetProperty((int)__VSPROPID.VSPROPID_SolutionDirectory, out solutionpath_o);
                 var solution_path = solutionpath_o as string;
-
+                
                 var files = CollectCTestTestfiles(solution_path);
 
                 return files.Select(f => new CTestContainer(this, f));
@@ -60,23 +62,30 @@ namespace ImplemenationCTestTestAdapter
         private IEnumerable<string> CollectCTestTestfiles(string currentDir)
         {
             var file = new FileInfo(currentDir + "/CTestTestfile.cmake");
-
-            var content = file.OpenText().ReadToEnd();
-            var matches = Regex.Matches(content, @".*[sS][uB][bB][dD][iI][rR][sS]\s*\((?<subdir>.*)\)");
-            var subdirs = new List<string>();
-            foreach (Match match in matches)
-            {
-                subdirs.Add(match.Groups["subdir"].Value);
-            }
             
-            if(subdirs.Count == 0 && content.Contains("add_test"))
+            if (file.Exists)
             {
-                return Enumerable.Repeat(file.FullName, 1);
+                var content = file.OpenText().ReadToEnd();
+                var matches = Regex.Matches(content, @".*[sS][uB][bB][dD][iI][rR][sS]\s*\((?<subdir>.*)\)");
+                var subdirs = new List<string>();
+                foreach (Match match in matches)
+                {
+                    subdirs.Add(match.Groups["subdir"].Value);
+                }
+                
+                if (subdirs.Count == 0 && content.Contains("add_test"))
+                {
+                    return Enumerable.Repeat(file.FullName, 1);
+                }
+                else
+                {
+                    return subdirs
+                        .SelectMany(d => CollectCTestTestfiles(currentDir + "/" + d));
+                }
             }
             else
             {
-                return subdirs
-                    .SelectMany(d => CollectCTestTestfiles(currentDir + "/" + d));
+                return Enumerable.Empty<string>();
             }
         }
 
